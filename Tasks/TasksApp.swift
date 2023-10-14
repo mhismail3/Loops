5e879456c0e234ba63828e2b86830285f8fd7836
Mohsin Ismail <mhismail3@gmail.com>
2023-10-14 02:23:42 -0500

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Moose on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
